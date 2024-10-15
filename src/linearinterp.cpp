0d0f145ab3846f6abbc6c73d860bb738b0396472
Armaan <armaan070903@gmail.com>
2024-10-14 17:36:50 -0700

    Changed entity to both move and rotate at startup

--- src/linearinterp.cpp
+++ src/linearinterp.cpp
@@ -8,6 +8,8 @@ bool isLoading = true;
 void interp_moveEntitesToScreen(GlRender& renderer) {
 	if (isLoading) {
 		Motion& m1 = registry.motions.get(renderer.m_player1);
+		PlayerInput& p1 = registry.playerInputs.get(renderer.m_player1);
+		p1 = PlayerInput();
 		if (m1.position[0] < -0.3) {
 			m1.position[0] += 0.0089;
 
@@ -18,6 +20,8 @@ void interp_moveEntitesToScreen(GlRender& renderer) {
 		}
 
 		Motion& m2 = registry.motions.get(renderer.m_player2);
+		PlayerInput& p2 = registry.playerInputs.get(renderer.m_player2);
+		p2 = PlayerInput();
 		if (m2.position[0] > 0.3) {
 			m2.position[0] -= 0.0089;
 
@@ -26,30 +30,32 @@ void interp_moveEntitesToScreen(GlRender& renderer) {
 			m2.position[1] -= 0.003455;
 
 		}
+		if (m1.position[0] < 0.3 && m1.position[1] < (FLOOR_Y + NDC_HEIGHT / 2)) {
+			count = 1;
+
+		}
 
 		// spin when you land
-		if (m1.position[0] >= -0.3) {
-			if (m1.angle > 0) {
-				a = m1.angle - 0.05;
-				m1.angle = a;
+		if (m1.angle > 0) {
+			a = m1.angle - 0.1;
+			m1.angle = a;
 
-			}
-			else if ((m1.angle <= 0) && (a != 0)){
-				m1.angle = 0;
-				count = 1;				
-			}
+		}
+		else if ((m1.angle <= 0) && (a != 0)){
+			m1.angle = 0;
+							
+		}
 
-			if (m2.angle > 0) {
-				a2 = m2.angle - 0.05;
-				m2.angle = a2;
-			}
-			else if (m2.angle == 0 && a2 != 0) {
-				m2.angle = 0;
-			}
-			if (count == 1) {
-				isLoading = false;
-				std::cout << "FINISHED" << std::endl;
-			}
+		if (m2.angle > 0) {
+			a2 = m2.angle - 0.1;
+			m2.angle = a2;
+		}
+		else if (m2.angle == 0 && a2 != 0) {
+			m2.angle = 0;
+		}
+		if (count == 1) {
+			isLoading = false;
+			std::cout << "FINISHED" << std::endl;
 		}
 
 	}
