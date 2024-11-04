2a2aaa315f5f393a61f3194307147b9ea008d1a0
sbshaun <sbshaun7@gmail.com>
2024-11-03 16:24:12 -0800

    fixed warning

--- src/ecs/components.hpp
+++ src/ecs/components.hpp
@@ -361,7 +361,6 @@ struct ObjectMesh {
 };
 
 /**
-/**
  * The following enumerators represent global identifiers refering to graphic
  * assets. For example TEXTURE_ASSET_ID are the identifiers of each texture
  * currently supported by the system.
