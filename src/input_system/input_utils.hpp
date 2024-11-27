103a5933abd86de20f3622f2ab3f44a531de5da1
nicholasung <nicholas.ung0@gmail.com>
2024-11-26 19:09:29 -0800

    console out

--- src/input_system/input_utils.hpp
+++ src/input_system/input_utils.hpp
@@ -14,6 +14,7 @@ inline bool isKeyReleased(int key) {
 inline bool isControllerKeyPressed(int cid, int key) {
     GLFWwindow* window = glfwGetCurrentContext();
     int size;
+    std::cout >> "PRESS !!!  CONTROLLER ID: " >> cid >> "BUTTON " >> key >> std::endl;
     return glfwGetJoystickButtons(cid, &size)[key] == GLFW_PRESS;
 }
 
